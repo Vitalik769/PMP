cced5ae19e1cb17bdc54ffdcaf121181cf529a6a
Vitalik769 <93341791+Vitalik769@users.noreply.github.com>
2023-11-07 22:40:47 +0200

    Add files via upload

---
+++ Components/Scroll.jsx
@@ -0,0 +1,121 @@
+import React, { useRef } from "react";
+import {
+  SafeAreaView,
+  ScrollView,
+  Text,
+  StyleSheet,
+  View,
+  ImageBackground,
+  Animated,
+  useWindowDimensions,
+} from "react-native";
+
+const images = [
+  "https://focus.ua/static/storage/thumbs/920x465/7/5f/b274d2a2-4dc57d06dd0d61277c07c738eb5ea5f7.jpg?v=1764_1",
+  "https://i.infocar.ua/i/2/4276/89992/1920x.jpg",
+  "https://auto-wedding.com.ua/photos/sedani/avto-na-svadbu-ford-mustang-light-green.jpg",
+  "https://img.automoto.ua/overview/ford-mustang-2023-99d-huge-2464.jpg",
+  "https://ford-vidi.com.ua/uploads/media/dc_car_gallery/0001/16/6340ad26c4bcbcaebb10673f9fc976be74f80237.jpeg",
+  "https://cdn1.riastatic.com/photosnew/auto/photo/ford_mustang__408911526f.jpg",
+];
+
+const App = () => {
+  const scrollX = useRef(new Animated.Value(0)).current;
+
+  const { width: windowWidth } = useWindowDimensions();
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.scrollContainer}>
+        <ScrollView
+          horizontal={true}
+          style={styles.scrollViewStyle}
+          pagingEnabled
+          showsHorizontalScrollIndicator={false}
+          onScroll={Animated.event([
+            {
+              nativeEvent: {
+                contentOffset: {
+                  x: scrollX,
+                },
+              },
+            },
+          ])}
+          scrollEventThrottle={1}
+        >
+          {images.map((image, imageIndex) => {
+            return (
+              <View
+                style={{ width: windowWidth, height: 250 }}
+                key={imageIndex}
+              >
+                <ImageBackground source={{ uri: image }} style={styles.card}>
+                </ImageBackground>
+              </View>
+            );
+          })}
+        </ScrollView>
+        <View style={styles.indicatorContainer}>
+          {images.map((image, imageIndex) => {
+            const width = scrollX.interpolate({
+              inputRange: [
+                windowWidth * (imageIndex - 1),
+                windowWidth * imageIndex,
+                windowWidth * (imageIndex + 1),
+              ],
+              outputRange: [8, 16, 8],
+              extrapolate: "clamp",
+            });
+            return (
+              <Animated.View
+                key={imageIndex}
+                style={[styles.normalDot, { width }]}
+              />
+            );
+          })}
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  scrollContainer: {
+    height: 300,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  card: {
+    flex: 1,
+    marginVertical: 4,
+    marginHorizontal: 16,
+    borderRadius: 5,
+    overflow: "hidden",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  infoText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  normalDot: {
+    height: 8,
+    width: 8,
+    borderRadius: 4,
+    backgroundColor: "silver",
+    marginHorizontal: 4,
+  },
+  indicatorContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export default App;
